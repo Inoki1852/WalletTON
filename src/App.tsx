6ad4fbf0da0f19ebf863da95cff14aa72e308d87
Bohdan <bogdangorlac@gmail.com>
2024-06-19 18:46:06 +0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,24 @@
+import React from 'react';
 import './App.css';
 import { TonConnectUIProvider } from '@tonconnect/ui-react';
-import { Route, Routes } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Home from './components/Home/Home';
 import Camera from './components/Camera/Camera';
 
 const App: React.FC = () => {
   return (
     <TonConnectUIProvider manifestUrl="https://telegram-mini-apps-bot.netlify.app/tonconnect-manifest.json">
-      <div className="App">
-        <header className="App-main">
-          <Routes>
-            <Route index element={<Home />} />
-            <Route path="/camera" element={<Camera />} />
-            <Route path="*" element={<h2>Error, Page not found</h2>} />
-          </Routes>
-        </header>
-      </div>
+      <Router>
+        <div className="App">
+          <header className="App-main">
+            <Routes>
+              <Route index element={<Home />} />
+              <Route path="/camera" element={<Camera />} />
+              <Route path="*" element={<h2>Error, Page not found</h2>} />
+            </Routes>
+          </header>
+        </div>
+      </Router>
     </TonConnectUIProvider>
   );
 };
